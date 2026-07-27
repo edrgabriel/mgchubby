e1638e90cc36cd0fb9daee8da053f9a1393fb5a5
edrgabriel <zer0cops100@gmail.com>
2026-07-27 17:52:32 -0300

    fix: corrige roteamento da pagina de assinatura usando Outlet do React Router v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter, Outlet } from 'react-router-dom';
 import { Header } from './components/layout/Header';
 import { BottomNav } from './components/layout/BottomNav';
 import { AssistancePage } from './pages/AssistancePage';
@@ -8,6 +8,18 @@ import { StorePage } from './pages/StorePage';
 import { CartPage } from './pages/CartPage';
 import { CustomerSignPage } from './pages/CustomerSignPage';
 import { CartProvider } from './contexts/CartContext';
+
+function MainLayout() {
+  return (
+    <div className="min-h-screen bg-brand-dark flex flex-col font-sans text-brand-light print:bg-white print:text-black">
+      <Header />
+      <main className="flex-1 w-full max-w-7xl mx-auto p-4 sm:p-6 print:p-0 print:m-0">
+        <Outlet />
+      </main>
+      <BottomNav />
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -19,20 +31,12 @@ function App() {
           <Route path="/assinar" element={<CustomerSignPage />} />
 
           {/* Layout principal do sistema */}
-          <Route path="/*" element={
-            <div className="min-h-screen bg-brand-dark flex flex-col font-sans text-brand-light print:bg-white print:text-black">
-              <Header />
-              <main className="flex-1 w-full max-w-7xl mx-auto p-4 sm:p-6 print:p-0 print:m-0">
-                <Routes>
-                  <Route path="/" element={<AssistancePage />} />
-                  <Route path="/dashboard" element={<DashboardPage />} />
-                  <Route path="/store" element={<StorePage />} />
-                  <Route path="/cart" element={<CartPage />} />
-                </Routes>
-              </main>
-              <BottomNav />
-            </div>
-          } />
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<AssistancePage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/store" element={<StorePage />} />
+            <Route path="/cart" element={<CartPage />} />
+          </Route>
         </Routes>
       </CartProvider>
     </BrowserRouter>
@@ -40,3 +44,4 @@ function App() {
 }
 
 export default App;
+
